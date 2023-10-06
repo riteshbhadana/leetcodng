a9dffdf573b6f8d9a1b8d56ea13d8ef6410513ee
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-10-06 22:03:07 +0530

    Update leetcoding.cpp

--- leetcoding.cpp
+++ leetcoding.cpp
@@ -50,3 +50,13 @@ public:
     }
 };
 ......................................................................
+  nim game
+    class Solution {
+public:
+    bool canWinNim(int n) {
+    if(n%4==0)
+    return 0;
+    else
+    return 1;    
+    }
+};
